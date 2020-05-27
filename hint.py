a6df3323d346d5779c507140a0ed19628c91c586
Rob Morris <rmorris1218@users.noreply.github.com>
2020-05-27 08:48:42 -0500

    correct function return, entrypoint arg

--- hint.py
+++ hint.py
@@ -2,10 +2,9 @@ from typing import Dict, List
 
 
 def tags_to_list(tags: Dict) -> List[Dict[str, str]]:
-    # return [{"Key": k, "Value": v} for k, v in tags.items()]
-    return None
+    return [{"Key": k, "Value": v} for k, v in tags.items()]
 
 
 if __name__ == "__main__":
-    tags = tags_to_list(tags="xxx")
+    tags = tags_to_list(tags={'k': 'v'})
     print("hello world")
